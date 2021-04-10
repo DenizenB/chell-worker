7d929fcbd3b246131db4d7b20da6a907b43d6dba
DenizenB <github@podd.xyz>
2021-04-10 16:31:04 +0200

    Add endpoint for urlhaus details

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // https://developers.cloudflare.com/workers/examples/cache-using-fetch
 
 const CACHE_TTL = 3600
+const CF_OPTS = {
+  cacheTtl: CACHE_TTL,
+  cacheEverything: true
+}
+
 const BLACKLISTS = {
   'urlhaus/all.csv.zip': "https://urlhaus.abuse.ch/downloads/csv/",
   'urlhaus/30days.csv': "https://urlhaus.abuse.ch/downloads/csv_recent/",
@@ -19,41 +24,50 @@ const BLACKLISTS = {
 }
 
 async function handleRequest(request) {
-  let url = new URL(request.url)
+  const url = new URL(request.url)
   const path = url.pathname.substr(1)
+  let match
 
   if (path in BLACKLISTS) {
     // Proxy request to abuse.ch blacklist
-    const resource = BLACKLISTS[path]
-    const response = await fetch(resource, {
-      cf: {
-        cacheTtl: CACHE_TTL,
-        cacheEverything: true
-      }
-    })
+    return fetch(BLACKLISTS[path], {cf: CF_OPTS})
+  }
 
-    return response
+  if (match = path.match(/urlhaus\/url\/(\d+)/)) {
+    // Fetch url details (in HTML because the API is throwing a fit)
+    const resource = "https://urlhaus.abuse.ch/url/" + match[1]
+    const response = await fetch(resource, {cf: CF_OPTS})
+    const body = await response.text()
+
+    // Extract payload hash
+    match = body.match(/<a href="\/browse\.php\?search=(.{64})/)
+    if (!match) {
+      return new Response(body, {status: 404})
+    }
+
+    return new Response(JSON.stringify({sha256: match[1]}), {
+        headers: {
+          'content-type': "application/json"
+        }
+      })
   }
 
   if (!path) {
     // List endpoints
-    let banner = `<h1>abuse.ch exports</h1>
-    <p>served by Cloudflare worker, 1 hour cache</p>
-    <ul>`
+    let banner = "<h1>abuse.ch exports</h1><p>cache TTL: 1 hour</p><ul>"
     for (item in BLACKLISTS) {
-      banner += '<li><a href="/' + item + '">' + item + '</a></li>'
+      banner += '<li><a href="/' + item + '">' + item + "</a></li>"
     }
-    banner += '</ul>'
+    banner += "</ul>"
 
     return new Response(banner, {
-      status: 200,
       headers: {
-        'Content-Type': 'text/html'
+        'Content-Type': "text/html"
       }
     })
   }
   
-  return new Response('no', {status: 404})
+  return new Response("no", {status: 404})
 }
 
 addEventListener('fetch', event => {
